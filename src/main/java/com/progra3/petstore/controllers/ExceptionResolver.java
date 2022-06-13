4578e3c5644e03d00f3ba5cbfbde317ad5928a40
VincetCampos <62453298+VincetCampos@users.noreply.github.com>
2022-06-12 19:29:51 -0600

    Se creo nueva clase para manejo de errores

---
+++ src/main/java/com/progra3/petstore/controllers/ExceptionResolver.java
@@ -0,0 +1,22 @@
+package com.progra3.petstore.controllers;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class ExceptionResolver {
+	@ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public HashMap<String, String> handleNoHandlerFound(NoHandlerFoundException e, WebRequest request) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "fail");
+        response.put("message", e.getLocalizedMessage());
+        return response;
+    }
+}
